ac96bb0cd9777d780091a108079c395668cde96d
sergioperez1998 <sergio-pm@hotmail.com>
2019-08-14 19:33:55 +0200

    lectura

---
+++ Meta-Algoritmo.py
@@ -0,0 +1,9 @@
+import pandas
+import numpy
+
+adults_train = pandas.read_csv('adult_train.csv', header=None,
+                       names=['age', 'capital-gain', 'capital-loss', 'hours-per-week',
+                              'workClass', 'education', 'marital-status','occupation',
+                              'relationship','race','sex','native-country','inconme'])
+
+print(adults_train.shape)  # El metodo shape muestra el numero de filas y columnas del .csv
