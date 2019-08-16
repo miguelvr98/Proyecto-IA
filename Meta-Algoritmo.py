62bbf5d12d88dc0f0f8ecdf9abfae4ce9dc4bef2
sergioperez1998 <sergio-pm@hotmail.com>
2019-08-16 17:35:10 +0200

    añadidas funciones para bootstrapping y rsm

--- Meta-Algoritmo.py
+++ Meta-Algoritmo.py
@@ -1,7 +1,12 @@
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 
-adults_train = pandas.read_csv('adult_train.csv', header=None,
+import random
+
+from decision_tree_functions import decision_tree_algorithm, decision_tree_predictions
+from helper_functions import train_test_split, calculate_accuracy
+
+adults_train = pd.read_csv('adult_train.csv', header=None,
                        names=['age', 'capital-gain', 'capital-loss', 'hours-per-week',
                               'workClass', 'education', 'marital-status','occupation',
                               'relationship','race','sex','native-country','inconme'])
