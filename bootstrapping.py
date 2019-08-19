31e55dcdefa6d265530e0ad2772b9461baa682d7
miguelvr98 <miguelvr98@gmail.com>
2019-08-19 17:21:02 +0200

    Resolviendo conflicto

--- bootstrapping.py
+++ bootstrapping.py
@@ -8,16 +8,11 @@ def bootstrapping(train, n_bootstrap):
     return df_bootstrapped
 
 def random_forest_algorithm(train, n_trees, n_bootstrap, n_features, dt_max_depth):
+    
     forest = []
-<<<<<<< HEAD
-    for _ in range(num_trees):
-        b = bootstrapping(adults_train,num_bootstrap)
-        tree = decision_tree_algorithm(b,max_depth=dt_max_depth,random_subspace=num_features)
-=======
     for i in range(n_trees):
         df_bootstrapped = bootstrapping(train, n_bootstrap)
         tree = decision_tree_algorithm(df_bootstrapped, max_depth=dt_max_depth, random_subspace=n_features)
->>>>>>> 14e69b4685d970ee495e6d99f1364f027982b14f
         forest.append(tree)
     
     return forest
